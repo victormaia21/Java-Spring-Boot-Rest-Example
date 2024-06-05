12c9011c4e61f5538b616569a3422eff96270575
victor <jv1555550@gmail.com>
2024-06-05 14:25:17 -0500

    .

--- src/main/java/com/victor_education/spring/config/OpenAPIConfiguration.java
+++ src/main/java/com/victor_education/spring/config/OpenAPIConfiguration.java
@@ -1,4 +1,5 @@
 package com.victor_education.spring.config;
+
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
@@ -9,25 +10,23 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 
-
 @Configuration
 public class OpenAPIConfiguration {
 
-   @Bean
-   public OpenAPI defineOpenApi() {
-       Server server = new Server();
-       server.setUrl("http://localhost:8080");
-       server.setDescription("Development");
+    @Bean
+    public OpenAPI defineOpenApi() {
+        Server server = new Server();
+        server.setUrl("http://localhost:8080");
+        server.setDescription("Development");
 
-       Contact myContact = new Contact();
-       myContact.setName("Jane Doe");
-       myContact.setEmail("your.email@gmail.com");
+        Contact myContact = new Contact();
+        myContact.setName("Jane Doe");
+        myContact.setEmail("your.email@gmail.com");
 
-       Info information = new Info()
-               .title("Employee Management System API")
-               .version("1.0")
-               .description("This API exposes endpoints to manage employees.")
-               .contact(myContact);
-       return new OpenAPI().info(information).servers(List.of(server));
-   }
+        Info information = new Info()
+                .title("Rest APi Java Example")
+                .version("1.0")
+                .contact(myContact);
+        return new OpenAPI().info(information).servers(List.of(server));
+    }
 }
